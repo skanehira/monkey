ca7addf88c2203814c2d9339357e573189445665
skanehira <sho19921005@gmail.com>
2021-08-14 15:58:48 +0900

    implement array

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -181,6 +181,15 @@ func (c *Compiler) Compile(node ast.Node) error {
 		default:
 			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
+	case *ast.ArrayLiteral:
+		for _, el := range node.Elements {
+			err := c.Compile(el)
+			if err != nil {
+				return err
+			}
+		}
+
+		c.emit(code.OpArray, len(node.Elements))
 	}
 	return nil
 }
